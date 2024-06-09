2b963f7cbf3340da9bbd42ce4c232988c725f32f
sebatustra <sebatustra@protonmail.com>
2024-06-09 16:44:34 -0400

    added the feature of commenting student intros, plus filtering those comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,21 +13,32 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import * as token from "@solana/spl-token";
-import { set } from '@coral-xyz/anchor/dist/cjs/utils/features';
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
 
 const WalletMultiButtonDynamic = dynamic(
     async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
     { ssr: false }
 );
 
-const PROGRAM_ID = new anchor.web3.PublicKey("8hkDLDXHJpXwEufzpUtWp5WhgVBvfoywcXApnpDXTEk2");
+const PROGRAM_ID = new anchor.web3.PublicKey("DQRdzv6NdrUEGWvMfbiqXDvFQSQ9kEnW76w2cpk84BmV");
 
 interface IntroAccount {
     publicKey: anchor.web3.PublicKey,
     account: {
         initializer: anchor.web3.PublicKey,
         name: string,
-        message: string
+        message: string,
+        count: number,
+        comments: IntroCommentAccount[]
+    }
+}
+
+interface IntroCommentAccount {
+    publicKey: anchor.web3.PublicKey,
+    account: {
+        intro: anchor.web3.PublicKey,
+        commenter: anchor.web3.PublicKey,
+        comment: string,
     }
 }
 
@@ -43,7 +54,8 @@ export default function Home() {
     const [newStudentName, setNewStudentName] = useState("");
     const [newStudentMessage, setNewStudentMessage] = useState("");
     const [studentIntros, setStudentIntros] = useState<IntroAccount[]>([])
-    const [updatedMessage, setUpdatedMessage] = useState("")
+    const [updatedMessage, setUpdatedMessage] = useState("");
+    const [introComment, setIntroComment] = useState("")
 
     useEffect(() => {
         if (wallet) {
@@ -59,8 +71,7 @@ export default function Home() {
                 )
             }
 
-            const programToSet = new anchor.Program(idl as anchor.Idl, provider)
-            setProgram(programToSet)
+            setProgram(new anchor.Program(idl as anchor.Idl, provider))
         }
     }, [wallet]);
 
@@ -128,20 +139,69 @@ export default function Home() {
         }
     }
 
+    const commentIntro = async (intro: IntroAccount) => {
+        if (program && wallet) {
+            try {
+                const [mintAddress] = anchor.web3.PublicKey.findProgramAddressSync(
+                    [
+                        Buffer.from("mint")
+                    ],
+                    PROGRAM_ID
+                );
+                const tokenAccount = await token.getAssociatedTokenAddress(
+                    mintAddress,
+                    wallet.publicKey,
+                );
+
+                await program.methods
+                    .addCommentToIntro(introComment)
+                    .accounts({
+                        commenter: wallet.publicKey,
+                        intro: intro.publicKey,
+                        tokenAccount
+                    })
+                    .rpc()
+
+                await fetchIntros()
+
+            } catch(e) {
+                console.error(e)
+            }
+        }
+    }
+
     const fetchIntros = async () => {
         if (program) {
             try {
                 const intros = await (program.account as any).introState.all();
-                let accounts: IntroAccount[] = intros.map((data: any) => {
-                    return {
+                let accounts: IntroAccount[] = await Promise.all(intros.map(async (data: any) => {
+
+                    const [commentCounterPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+                        [
+                            Buffer.from("counter"),
+                            data.publicKey.toBuffer()
+                        ],
+                        program.programId
+                    )
+
+                    const counter = await program.account.commentCounterState.fetch(commentCounterPDA);
+
+                    const comments = await fetchIntroComments(data.publicKey)
+
+                    let intro = {
                         publicKey: data.publicKey,
                         account: {
                             initializer: new anchor.web3.PublicKey(data.account.initializer),
                             message: data.account.message,
-                            name: data.account.name
+                            name: data.account.name,
+                            count: Number(counter.count),
+                            comments: comments ? comments : []
                         }
                     }
-                })
+                    console.log("intro:", intro)
+                    return intro
+                    
+                }))
                 setStudentIntros(accounts)
             } catch(e) {
                 console.error(e)
@@ -156,13 +216,17 @@ export default function Home() {
             ],
             PROGRAM_ID
         );
-        const mint = await token.getMint(
-            connection,
-            mintAddress
-        );
-
-        setIsMintInitialized(mint.isInitialized)
-        setTokenSupply(Number(mint.supply))
+        try {
+            const mint = await token.getMint(
+                connection,
+                mintAddress
+            );
+            setIsMintInitialized(mint.isInitialized)
+            setTokenSupply(Number(mint.supply))
+        } catch {
+            setIsMintInitialized(false)
+            setTokenSupply(0)
+        }
     }
 
     const updateIntro = async (intro: IntroAccount) => {
@@ -184,27 +248,42 @@ export default function Home() {
         }
     }
 
-    const closeIntro = (intro: IntroAccount) => {
-        const execute = async () => {
-            try {
-                if (program) {
-                    await program.methods
-                        .closeStudentIntro(
-                            intro.account.name
-                        )
-                        .accounts({
-                            intro: intro.publicKey
-                        })
-                        .rpc()
+    const closeIntro = async (intro: IntroAccount) => {
+        try {
+            if (program) {
+                await program.methods
+                    .closeStudentIntro(
+                        intro.account.name
+                    )
+                    .accounts({
+                        intro: intro.publicKey
+                    })
+                    .rpc()
 
-                    fetchIntros()
-                }
+                fetchIntros()
+            }
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    const fetchIntroComments = async (publicKey: anchor.web3.PublicKey) => {
+        if (program) {
+            try {
+                const comments: IntroCommentAccount[] = await program.account.introCommentState.all([
+                    {
+                        memcmp: {
+                            offset: 8,
+                            bytes: bs58.encode(publicKey.toBuffer())
+                        }
+                    }
+                ])
+
+                return comments
             } catch(e) {
                 console.error(e)
             }
         }
-
-        execute()
     }
 
 
@@ -213,7 +292,7 @@ export default function Home() {
             <div className="fixed top-0 right-0 p-4">
                 <WalletMultiButtonDynamic />
             </div>
-            <Card className='w-[500px]'>
+            <Card className='w-[600px]'>
                 <CardHeader>
                     <CardTitle>
                         {isMintInitialized ? "Mint Supply:" : "Initialize Mint"}
@@ -229,7 +308,7 @@ export default function Home() {
                     )}
                 </CardContent>
             </Card>
-            <Card className='w-[500px]'>
+            <Card className='w-[600px]'>
                 <CardHeader>
                     <CardTitle>
                         New Student Intro
@@ -268,11 +347,18 @@ export default function Home() {
                 </CardHeader>
                 <CardContent className="flex flex-col gap-y-2">
                     {studentIntros.map(intro => (
-                        <Card>
+                        <Card key={intro.publicKey.toString()}>
                             <CardContent className='flex flex-col w-full gap-y-1 p-2'>
                                 <p>Initializer: {intro.account.initializer.toString()}</p>
                                 <p>Student name: {intro.account.name}</p>
                                 <p>Student message: {intro.account.message}</p>
+                                <p>Current comment count: {intro.account.count}</p>
+                                {intro.account.comments && intro.account.comments.map( comment => (
+                                    <div key={comment.publicKey.toString()}>
+                                        <p>comment: {comment.account.comment}</p>
+                                        <p>commenter: {comment.account.commenter.toString()}</p>
+                                    </div>
+                                ))}
                                 <div className='flex flex-row gap-x-2'>
                                     <Popover>
                                         <PopoverTrigger>
@@ -286,6 +372,23 @@ export default function Home() {
                                             />
                                             <Button
                                                 onClick={() => updateIntro(intro)}
+                                            >
+                                                Submit
+                                            </Button>
+                                        </PopoverContent>
+                                    </Popover>
+                                    <Popover>
+                                        <PopoverTrigger>
+                                            Comment
+                                        </PopoverTrigger>
+                                        <PopoverContent className='flex flex-col gap-y-1'>
+                                            New comment:
+                                            <Input
+                                                value={introComment}
+                                                onChange={(event) => setIntroComment(event.target.value)}
+                                            />
+                                            <Button
+                                                onClick={() => commentIntro(intro)}
                                             >
                                                 Submit
                                             </Button>
